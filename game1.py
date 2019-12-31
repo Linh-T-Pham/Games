6c990f488d5d43c1dc4b13ef74d893f973addd24
Linh Pham <thuytpham9@gmail.com>
2019-12-30 23:39:15 -0800

    Set up the game

---
+++ game1.py
@@ -0,0 +1,52 @@
+def make_move(board):
+    """Asks a player for a move and adds the move to the board."""
+
+    pass
+
+    # asks user for their player #
+
+
+    # asks user for a row
+
+
+    # asks user for a column
+
+
+    # check if board at row and column has been taken, else put x or o there depending on player
+
+
+def print_board(board):
+    """Prints board to show the players"""
+
+    pass
+
+
+def check_win(board):
+    """Checks board for a winner and returns winner"""
+
+    pass
+
+    # checks rows for win
+
+
+    # checks columns for win
+
+
+    # checks diagonals for win
+
+
+def check_tie(board):
+    """Checks if board is full and there's a tie"""
+
+    pass
+
+
+def play(board):
+    """Main REPL function to play the game"""
+
+    pass
+
+
+#################################################################################
+tic_tac_toe_board = [["-","-","-"],["-","-","-"],["-","-","-"]]
+play(tic_tac_toe_board)
